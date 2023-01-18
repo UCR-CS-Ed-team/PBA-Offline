782bbb506133298a61e8a4e5f51d65e3b10b6003
Benjamin Denzler <bdenz001@ucr.edu>
2023-01-18 00:05:21 -0800

    Added three more anomalies for ChatGPT
    Added Max Min Macros, Swap Function, and Cin Inside While style anomalies to better detect ChatGPT solutions. Also fixed an oversight in the Brace Styling anomaly.

--- tools/anomaly.py
+++ tools/anomaly.py
@@ -167,7 +167,7 @@ BRACE_STYLING_REGEX = r"""
 	((?P<leftbrace>^(?:\s+)?{)
 	|(?P<rightbrace>\S+;(?:\s+)?})
 	|(?P<oneline>(?:if|for)(?:\s+)?\(.+\)(?:\s+)?\S+.*;)
-	|(?P<else1>}(?:\s+)?else(?:\s+)?{)
+	|(?P<else1>}(?:\s+)?else)
 	|(?P<else2>else\s\S+.*;))
 	"""
 ARRAY_ACCESSES_REGEX = r"(\w+\[.*\])"
@@ -195,6 +195,9 @@ AUTO_REGEX = r"(auto\s\w+)"
 SET_PRECISION_REGEX = r"cout(?:\s+)?<<(?:\s+)?setprecision\(.+\)(?:\s+)?<<(?:\s+)?fixed"
 RANGED_BASED_LOOP_REGEX = r"(for\s*\(\s*\w+\s+\w+\s+:\s+\w+\s*\))"
 ITERATOR_FUNCTIONS_REGEX = r"(\w+\.(?:begin|end)\(\))"
+MAX_MIN_MACRO_REGEX = r"(INT_MAX|INT_MIN)"
+SWAP_FUNCTION_REGEX = r"(swap\(.*\))"
+CIN_INSIDE_WHILE_REGEX = r"(while\s*\(.*cin\s*>>\s*\w+.*\))"
 
 # Helper regular expressions
 INT_MAIN_REGEX = r"int main(?:\s+)?\(.*\)"
@@ -233,7 +236,10 @@ def get_anomaly_score(code, auto=0):
         'Auto': {0:1, 1:0.3, 2:1, 3:AUTO_REGEX, 4:0, 5:-1},
         'zyBooks Set Precision': {0:1, 1:0.2, 2:1, 3:SET_PRECISION_REGEX, 4:0, 5:-1},
         'Ranged-Based for Loop': {0:1, 1:0.6, 2:1, 3:RANGED_BASED_LOOP_REGEX, 4:0, 5:-1},
-        'Iterator Functions': {0:1, 1:0.3, 2:1, 3:ITERATOR_FUNCTIONS_REGEX, 4:0, 5:-1}
+        'Iterator Functions': {0:1, 1:0.3, 2:1, 3:ITERATOR_FUNCTIONS_REGEX, 4:0, 5:-1},
+        'Max Min Macros': {0:1, 1:0.3, 2:1, 3:MAX_MIN_MACRO_REGEX, 4:0, 5:-1},
+        'Swap Function': {0:1, 1:0.6, 2:1, 3:SWAP_FUNCTION_REGEX, 4:0, 5:-1},
+        'Cin Inside While': {0:1, 1:0.6, 2:1, 3:CIN_INSIDE_WHILE_REGEX, 4:0, 5:-1}
     }
     
     anomaly_score = 0           # Initial anomaly Score
@@ -366,6 +372,33 @@ def get_anomaly_score(code, auto=0):
                     if (Styleanomaly['Scope Operator'][5] <= -1) or (Styleanomaly['Scope Operator'][5] > -1 and (Styleanomaly['Scope Operator'][4] < Styleanomaly['Scope Operator'][5])):
                         anomaly_score += Styleanomaly['Scope Operator'][1]
                     Styleanomaly['Scope Operator'][4] += 1
+                    anomalies_found += 1
+
+        if Styleanomaly['Max Min Macros'][2] != 0: #Check if the anomaly is turned on 
+            if re.search(Styleanomaly['Max Min Macros'][3], line):
+                if Styleanomaly['Max Min Macros'][0] == 1 or (Styleanomaly['Max Min Macros'][0] == 0 and Styleanomaly['Max Min Macros'][4] == 0):
+                    # Score for this anomaly is capped to Styleanomaly[x][5] instances
+                    if (Styleanomaly['Max Min Macros'][5] <= -1) or (Styleanomaly['Max Min Macros'][5] > -1 and (Styleanomaly['Max Min Macros'][4] < Styleanomaly['Max Min Macros'][5])):
+                        anomaly_score += Styleanomaly['Max Min Macros'][1]
+                    Styleanomaly['Max Min Macros'][4] += 1
+                    anomalies_found += 1
+
+        if Styleanomaly['Swap Function'][2] != 0: #Check if the anomaly is turned on 
+            if re.search(Styleanomaly['Swap Function'][3], line):
+                if Styleanomaly['Swap Function'][0] == 1 or (Styleanomaly['Swap Function'][0] == 0 and Styleanomaly['Swap Function'][4] == 0):
+                    # Score for this anomaly is capped to Styleanomaly[x][5] instances
+                    if (Styleanomaly['Swap Function'][5] <= -1) or (Styleanomaly['Swap Function'][5] > -1 and (Styleanomaly['Swap Function'][4] < Styleanomaly['Swap Function'][5])):
+                        anomaly_score += Styleanomaly['Swap Function'][1]
+                    Styleanomaly['Swap Function'][4] += 1
+                    anomalies_found += 1
+
+        if Styleanomaly['Cin Inside While'][2] != 0: #Check if the anomaly is turned on 
+            if re.search(Styleanomaly['Cin Inside While'][3], line):
+                if Styleanomaly['Cin Inside While'][0] == 1 or (Styleanomaly['Cin Inside While'][0] == 0 and Styleanomaly['Cin Inside While'][4] == 0):
+                    # Score for this anomaly is capped to Styleanomaly[x][5] instances
+                    if (Styleanomaly['Cin Inside While'][5] <= -1) or (Styleanomaly['Cin Inside While'][5] > -1 and (Styleanomaly['Cin Inside While'][4] < Styleanomaly['Cin Inside While'][5])):
+                        anomaly_score += Styleanomaly['Cin Inside While'][1]
+                    Styleanomaly['Cin Inside While'][4] += 1
                     anomalies_found += 1
 
         # "Line Spacing" should only check lines inside main() and user functions
