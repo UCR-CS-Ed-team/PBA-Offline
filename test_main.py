6fbb3f87217a1345f98c39e382582b24f5afd609
ben-denzler <bdenz001@ucr.edu>
2023-11-03 14:38:42 -0700

    Updated test_main.py

--- test_main.py
+++ test_main.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
-file_prefixes = ['quickanalysis', 'basic_stats_roster', 'anomalies']
+file_prefixes = ['quickanalysis', 'basic-stats-roster', 'anomalies']
 
 for prefix in file_prefixes:
-	df1 = pd.read_csv(f'output_testing/{prefix}-pre.csv')
-	df2 = pd.read_csv(f'output_testing/{prefix}-post.csv')
+	pre = pd.read_csv(f'output_testing/{prefix}-pre.csv')
+	post = pd.read_csv(f'output_testing/{prefix}-post.csv')
 
-	if df1.equals(df2):
+	if pre.equals(post):
 		print(f'✓ The {prefix} files are the same.')
 	else:
-		print(f'✗ The {prefix} files are different.')
+		print(f'✗ The {prefix} files are different:')
+		print(pre.compare(post))
