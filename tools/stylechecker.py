62afca23b823a6c3cfe04af526dac800553365e4
ben-denzler <bdenz001@ucr.edu>
2024-04-25 21:56:24 -0700

    Updated docs in `stylechecker.py`

--- tools/stylechecker.py
+++ tools/stylechecker.py
@@ -4,29 +4,27 @@ import subprocess
 cpplint_file = 'output/code.cpp'
 
 
-def stylechecker(data, selected_labs):
-    """
-    stylechecker_output from user defined function structure
-    Input:
-    ------
-        Accepts the students data and selected labs as an input
+def stylechecker(data: dict, selected_labs: list[float]) -> dict:
+    """Check each student's code style with cpplint and return its result.
 
-    Output:
-    -------
-        stylechecker_output = {
-            student_id : {
-                'Lab 1' : [style_score, output1, code],
-                    ...
-                'Lab 2' : [style_score, output1, code],
-                    ...
-                'Lab n' : [style_score, output1, code],
-                    ...
-            }
+    Args:
+        data (dict): The log of all student submissions.
+        selected_labs (list[float]): A list of lab IDs to consider.
+
+    Returns:
+        dict: A dictionary containing the stylechecker output for each student and lab.
+
+    Example:
+        student_id : {
+            'Lab 1' : [style_score, output1, code],
+            'Lab 2' : [style_score, output1, code],
+            'Lab n' : [style_score, output1, code],
+            ...
         }
 
     Note:
-        Todo: This currently creates a new .cpp file and runs the package on that, modify it in
-                    such a way to avoid creating a new file and directly use the code in data
+        This function currently creates a new .cpp file and runs cpplint on that.
+        TODO: Modify it to instead directly use the student's code from the logfile.
     """
     output = {}
     for lab in selected_labs:
